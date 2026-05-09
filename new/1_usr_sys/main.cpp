115bb326150957653396335532d0ad20527497a9
Gousev Ivan <vanyagousev@gmail.com>
2026-05-09 16:33:37 +0300

    formatting

--- new/1_usr_sys/main.cpp
+++ new/1_usr_sys/main.cpp
@@ -7,43 +7,36 @@
 #include <string>
 
 class User;
-class Group
-{
+class Group {
 public:
   int id = 0;
-  std::set<User*> users{};
+  std::set<User *> users{};
   ~Group();
 };
 
-class User
-{
+class User {
 public:
   int id = 0;
   std::string name = "";
   std::string info = "";
-  std::set<Group*> groups{};
+  std::set<Group *> groups{};
   User() = default;
   User(int id, std::string name, std::string info)
-    : id(id)
-    , name(name)
-    , info(info) {};
-  ~User()
-  {
-    for (auto& i : groups) {
+      : id(id), name(name), info(info) {};
+  ~User() {
+    for (auto &i : groups) {
       i->users.erase(this);
     }
   }
 };
 
-Group::~Group()
-{
-  for (auto& i : users) {
+Group::~Group() {
+  for (auto &i : users) {
     i->groups.erase(this);
   }
 }
 
-enum Modes
-{
+enum Modes {
   createUser,
   deleteUser,
   allUsers,
@@ -55,9 +48,7 @@ enum Modes
   addUser,
   err__
 };
-int
-hash_modes(std::string mode)
-{
+int hash_modes(std::string mode) {
   if (mode == "createUser")
     return createUser;
   if (mode == "deleteUser")
@@ -78,9 +69,7 @@ hash_modes(std::string mode)
     return getGroup;
   return err__;
 }
-std::string
-get_info()
-{
+std::string get_info() {
   std::string s{};
   char c = std::cin.get();
   if (c == '\n' || !c) {
@@ -94,9 +83,7 @@ get_info()
   return s;
 }
 
-auto
-user_info(User& usr)
-{
+auto user_info(User &usr) {
   auto it = usr.groups.begin();
   std::stringstream msg;
   if (it != usr.groups.end()) {
@@ -112,18 +99,14 @@ user_info(User& usr)
   return msg.str();
 }
 
-bool
-clear_cin(char to = '\n')
-{
+bool clear_cin(char to = '\n') {
   std::cin.clear();
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), to);
   std::cout << "something gone wrong" << std::endl;
   return false;
 }
 
-int
-main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   std::map<int, Group> groups;
   std::map<int, User> users;
   int idu, idg;
@@ -134,126 +117,122 @@ main(int argc, char* argv[])
       std::cout << ">>> ";
       std::cin >> mode;
       switch (hash_modes(mode)) {
-        case createUser:
-          if (!(std::cin >> idu >> name || clear_cin()))
-            break;
+      case createUser:
+        if (!(std::cin >> idu >> name || clear_cin()))
+          break;
 
-          if (users.find(idg) != users.end()) {
-            std::cout << "ERROR: user already exist" << std::endl;
-            break;
-          }
-          info = get_info();
-          users.emplace(idu, User(idu, name, info));
-          std::cout << std::format("creating user id:{} name:{} info:\"{}\" ",
-                                   idu,
-                                   name,
-                                   info)
+        if (users.find(idg) != users.end()) {
+          std::cout << "ERROR: user already exist" << std::endl;
+          break;
+        }
+        info = get_info();
+        users.emplace(idu, User(idu, name, info));
+        std::cout << std::format("creating user id:{} name:{} info:\"{}\" ",
+                                 idu, name, info)
+                  << std::endl;
+        break;
+      case deleteUser:
+        if (!(std::cin >> idu || clear_cin()))
+          break;
+
+        if (users.find(idu) != users.end()) {
+          User &usr = users[idu];
+          std::cout << std::format("deleting user id:{} name:{} info:\"{}\" ",
+                                   usr.id, usr.name, usr.info)
                     << std::endl;
+          users.erase(idu);
+        } else {
+          std::cout << "ERROR: user with id " << idu << "does not exists"
+                    << std::endl;
+        }
+        break;
+      case allUsers:
+        std::cout << "id\tname\tgroups\tinfo\n";
+        for (auto &i : users) {
+          std::cout << user_info(i.second) << std::endl;
+        }
+        std::cout << std::endl;
+        break;
+      case getUser:
+        if (!(std::cin >> idu || clear_cin()))
           break;
-        case deleteUser:
-          if (!(std::cin >> idu || clear_cin()))
-            break;
 
-          if (users.find(idu) != users.end()) {
-            User& usr = users[idu];
-            std::cout << std::format("deleting user id:{} name:{} info:\"{}\" ",
-                                     usr.id,
-                                     usr.name,
-                                     usr.info)
-                      << std::endl;
-            users.erase(idu);
-          } else {
-            std::cout << "ERROR: user with id " << idu << "does not exists"
-                      << std::endl;
-          }
-          break;
-        case allUsers:
+        if (users.find(idu) != users.end()) {
           std::cout << "id\tname\tgroups\tinfo\n";
-          for (auto& i : users) {
-            std::cout << user_info(i.second) << std::endl;
-          }
-          std::cout << std::endl;
+          std::cout << user_info(users[idu]) << "\n" << std::endl;
+        } else
+          std::cout << "ERROR: user with id " << idu << "does not exists"
+                    << std::endl;
+        break;
+      case createGroup:
+        if (!(std::cin >> idg || clear_cin()))
           break;
-        case getUser:
-          if (!(std::cin >> idu || clear_cin()))
-            break;
-
-          if (users.find(idu) != users.end()) {
-            std::cout << "id\tname\tgroups\tinfo\n";
-            std::cout << user_info(users[idu]) << "\n" << std::endl;
-          } else
-            std::cout << "ERROR: user with id " << idu << "does not exists"
-                      << std::endl;
+        if (groups.find(idg) != groups.end()) {
+          std::cout << "ERROR: group already exist" << std::endl;
           break;
-        case createGroup:
-          if (!(std::cin >> idg || clear_cin()))
-            break;
-          if (groups.find(idg) != groups.end()) {
-            std::cout << "ERROR: group already exist" << std::endl;
-            break;
-          }
-          groups[idg] = Group{ idg };
-          std::cout << "created group with id:" << idg << std::endl;
+        }
+        groups[idg] = Group{idg};
+        std::cout << "created group with id:" << idg << std::endl;
+        break;
+      case addUser:
+        if (!(std::cin >> idu >> idg || clear_cin()))
           break;
-        case addUser:
-          if (!(std::cin >> idu >> idg || clear_cin()))
-            break;
-          if (groups.find(idg) == groups.end()) {
-            std::cout << "ERROR: invalid group" << std::endl;
-            break;
-          }
-          groups[idg].users.emplace(&users[idu]);
-          users[idu].groups.emplace(&groups[idg]);
+        if (groups.find(idg) == groups.end()) {
+          std::cout << "ERROR: invalid group" << std::endl;
           break;
-        case deleteGroup:
-          if (!(std::cin >> idg || clear_cin()))
-            break;
-          if (groups.find(idg) == groups.end()) {
-            std::cout << "ERROR: group does not exists" << std::endl;
-            break;
-          }
-          groups.erase(idg);
-          std::cout << "deleated group" << idg << std::endl;
+        }
+        groups[idg].users.emplace(&users[idu]);
+        users[idu].groups.emplace(&groups[idg]);
+        break;
+      case deleteGroup:
+        if (!(std::cin >> idg || clear_cin()))
           break;
-        case allGroups:
-          if (groups.size()) {
-            std::cout << "group:\t users\n";
-            for (auto& i : groups) {
-              std::cout << i.second.id << ":\t";
-              if (i.second.users.size()) {
-                for (auto& j : i.second.users) {
-                  std::cout << " " << j->id;
-                }
-                std::cout << std::endl;
-              } else {
-                std::cout << " None" << std::endl;
+        if (groups.find(idg) == groups.end()) {
+          std::cout << "ERROR: group does not exists" << std::endl;
+          break;
+        }
+        groups.erase(idg);
+        std::cout << "deleated group" << idg << std::endl;
+        break;
+      case allGroups:
+        if (groups.size()) {
+          std::cout << "group:\t users\n";
+          for (auto &i : groups) {
+            std::cout << i.second.id << ":\t";
+            if (i.second.users.size()) {
+              for (auto &j : i.second.users) {
+                std::cout << " " << j->id;
               }
+              std::cout << std::endl;
+            } else {
+              std::cout << " None" << std::endl;
             }
-          } else {
-            std::cout << "there is no groups to show" << std::endl;
           }
-          break;
-        case getGroup:
-          if (!(std::cin >> idg || clear_cin()))
-            break;
-
-          if (groups.find(idg) != groups.end()) {
-            std::cout << "group:\t users\n";
-            std::cout << idg << ":\t";
-            for (auto& j : groups[idg].users) {
-              std::cout << " " << j->id;
-            }
-            if (!groups[idg].users.size())
-              std::cout << " None";
-            std::cout << std::endl;
-          } else
-            std::cout << "ERROR: user with id " << idu << "does not exists"
-                      << std::endl;
+        } else {
+          std::cout << "there is no groups to show" << std::endl;
+        }
+        break;
+      case getGroup:
+        if (!(std::cin >> idg || clear_cin()))
           break;
 
-        default:
-          clear_cin();
-          std::cout << "command \"" << mode << "\" is invalid" << std::endl;
+        if (groups.find(idg) != groups.end()) {
+          std::cout << "group:\t users\n";
+          std::cout << idg << ":\t";
+          for (auto &j : groups[idg].users) {
+            std::cout << " " << j->id;
+          }
+          if (!groups[idg].users.size())
+            std::cout << " None";
+          std::cout << std::endl;
+        } else
+          std::cout << "ERROR: user with id " << idu << "does not exists"
+                    << std::endl;
+        break;
+
+      default:
+        clear_cin();
+        std::cout << "command \"" << mode << "\" is invalid" << std::endl;
       }
 
     } catch (...) {
